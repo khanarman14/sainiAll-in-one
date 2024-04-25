d0b67c5c93fae1a832460c889ccb718de687f2b6
Nickel18 <167849937+Nickel18@users.noreply.github.com>
2024-04-25 17:23:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = "6445385204:AAGaYbgY4KioA7VW0O41encp2uWdLcE-e5g"
-    API_ID = "23581132"
-    API_HASH = "f600844adba85094cfc1a608e2e737c0"
-    AUTH_USERS = '5422510270,1227089641'
+    BOT_TOKEN = "6947491658:AAH-4t_tQCt8iPXeIbUpNLQR-HnsxtQhfvQ"
+    API_ID = "20963688"
+    API_HASH = "964a26dc404aa5e12dfe82ea94ae7ae3"
+    AUTH_USERS = '6867189163'
 
